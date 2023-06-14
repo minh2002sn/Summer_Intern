ba0cea5a41a32b1584a2fe99c266eefab570fa2d
minh2002sn <minhphngnht@gmail.com>
2023-06-14 11:09:14 +0700

    Change folder structure

--- 3_01_Lab/include/input/input.h
+++
@@ -1,6 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-void input_handle(int *num_people);
-
-#endif /* INPUT_H */
--- 3_01_Lab/include/output/output.h
+++
@@ -1,8 +0,0 @@
-#ifndef OUTPUT_H
-#define OUTPUT_H
-
-#include <stdint.h>
-
-int output_handle(uint8_t num_people);
-
-#endif /* OUTPUT_H */
